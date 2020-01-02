25b69c80f9bfb2637a4b31b96a3b95854dd2d1f1
the-hof <genghisuber@gmail.com>
2020-01-02 16:21:19 -0500

    moved market cap caching to FinanceAPI class

--- bot_engine/executors/finance.py
+++ bot_engine/executors/finance.py
@@ -27,6 +27,60 @@ class FinanceAPI():
         except Exception as e:
             print(e)
 
+    def get_market_caps(self, symbols):
+        """Interpret a query for market cap.
+
+        Returns
+        -------
+          List of tuple (symbol, name, market_cap_raw, market_cap)
+        """
+        uncached_symbols = []
+        stocks = []
+        for symbol in symbols:
+            state_manager = StateManager()
+            stock_data = state_manager.get_key_value(f"SYMBOL_{symbol}")
+            if stock_data != {}:
+                print(f"using cached data for {symbol}")
+                stocks.append(stock_data)
+            else:
+                uncached_symbols.append(symbol)
+        try:
+            stocks += self.make_stock_request(uncached_symbols)
+        except Exception as e:
+            print (e)
+
+        market_cap_return = []
+        if stocks:
+            for stock in stocks:
+                symbol = stock.get("symbol", "")
+                print (f"... evaluating stock data for {symbol}")
+                market_cap_raw = stock.get("market_cap", -1)
+                print (f"... market cap = {market_cap_raw}")
+                market_cap = format_significance(market_cap_raw)
+                print (f"... market cap = {market_cap}")
+                name = stock.get("name", "")
+
+                try:
+                    cache_key = f"SYMBOL_{symbol}"
+                    cache_value = {
+                        "symbol": symbol,
+                        "market_cap": market_cap_raw,
+                        "name": name,
+                        "refresh_date": f"{datetime.today().strftime('%Y-%m-%d')}"
+                    }
+                except Exception as e:
+                    print (e)
+
+                try:
+                    state_manager.save_key_value(cache_key, cache_value)
+                except Exception as e:
+                    print (e)
+
+                if int(market_cap_raw) > 0:
+                    market_cap_return.append((symbol, name, market_cap_raw, market_cap))
+
+        return market_cap_return
+
 def format_significance(number, significance=2):
     work_number = int(number)
     suffix = ""
@@ -41,55 +95,62 @@ def format_significance(number, significance=2):
     rounded = display_number + suffix
     return rounded
 
+# def ask_market_cap(symbols):
+#     """Interpret a query for market cap."""
+#     uncached_symbols = []
+#     stocks = []
+#     for symbol in symbols:
+#         state_manager = StateManager()
+#         stock_data = state_manager.get_key_value(f"SYMBOL_{symbol}")
+#         if stock_data != {}:
+#             print(f"using cached data for {symbol}")
+#             stocks.append(stock_data)
+#         else:
+#             uncached_symbols.append(symbol)
+#     try:
+#         stocks += FinanceAPI().make_stock_request(uncached_symbols)
+#     except Exception as e:
+#         print (e)
+
+#     if stocks:
+#         text = ""
+#         for stock in stocks:
+#             symbol = stock.get("symbol", "")
+#             print (f"... evaluating stock data for {symbol}")
+#             market_cap_raw = stock.get("market_cap", -1)
+#             print (f"... market cap = {market_cap_raw}")
+#             market_cap = format_significance(market_cap_raw)
+#             print (f"... market cap = {market_cap}")
+#             name = stock.get("name", "")
+
+#             try:
+#                 cache_key = f"SYMBOL_{symbol}"
+#                 cache_value = {
+#                     "symbol": symbol,
+#                     "market_cap": market_cap_raw,
+#                     "name": name,
+#                     "refresh_date": f"{datetime.today().strftime('%Y-%m-%d')}"
+#                 }
+#             except Exception as e:
+#                 print (e)
+
+#             try:
+#                 state_manager.save_key_value(cache_key, cache_value)
+#             except Exception as e:
+#                 print (e)
+
+#             if int(market_cap_raw) > 0:
+#                 text += f"Market cap for {name} ({symbol}) is {market_cap}\n"
+#             else:
+#                 text += f"I can't find the information for {symbol}.\n"
+#     print (f"final text = {text}")
+#     return text
+
 def ask_market_cap(symbols):
-    """Interpret a query for market cap."""
-    uncached_symbols = []
-    stocks = []
-    for symbol in symbols:
-        state_manager = StateManager()
-        stock_data = state_manager.get_key_value(f"SYMBOL_{symbol}")
-        if stock_data != {}:
-            print(f"using cached data for {symbol}")
-            stocks.append(stock_data)
-        else:
-            uncached_symbols.append(symbol)
-    try:
-        stocks += FinanceAPI().make_stock_request(uncached_symbols)
-    except Exception as e:
-        print (e)
-
-    if stocks:
-        text = ""
-        for stock in stocks:
-            symbol = stock.get("symbol", "")
-            print (f"... evaluating stock data for {symbol}")
-            market_cap_raw = stock.get("market_cap", -1)
-            print (f"... market cap = {market_cap_raw}")
-            market_cap = format_significance(market_cap_raw)
-            print (f"... market cap = {market_cap}")
-            name = stock.get("name", "")
-
-            try:
-                cache_key = f"SYMBOL_{symbol}"
-                cache_value = {
-                    "symbol": symbol,
-                    "market_cap": market_cap_raw,
-                    "name": name,
-                    "refresh_date": f"{datetime.today().strftime('%Y-%m-%d')}"
-                }
-            except Exception as e:
-                print (e)
-
-            try:
-                state_manager.save_key_value(cache_key, cache_value)
-            except Exception as e:
-                print (e)
-
-            if int(market_cap_raw) > 0:
-                text += f"Market cap for {name} ({symbol}) is {market_cap}\n"
-            else:
-                text += f"I can't find the information for {symbol}.\n"
-    print (f"final text = {text}")
+    market_caps = FinanceAPI().get_market_caps(symbols)
+    text = ""
+    for (symbol, name, market_cap_raw, market_cap) in market_caps:
+        text += f"Market cap for {name} ({symbol}) is {market_cap}\n"
     return text
 
 def ask_news(symbol):
